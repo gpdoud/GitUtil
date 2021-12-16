86bafc4347122d2ed57e37a18ac8d1f0c6c3ffe1
Greg Doud <gpdoud@gmail.com>
2021-12-15 20:39:06 -0500

    Committed by GitUtil

---
+++ Dsi.GitUtil.Security/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsi.GitUtil.Security {
+    
+    public class Tokens {
+
+        public static string GitHub = "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";
+
+    }
+}
