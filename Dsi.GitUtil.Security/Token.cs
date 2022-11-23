294af10e8ef0e627c5f863cd302d0854bdd3862f
Greg Doud <gdoud@gmail.com>
2022-11-23 15:52:06 -0500

    commit for latop reset

--- Dsi.GitUtil.Security/Token.cs
+++ Dsi.GitUtil.Security/Token.cs
@@ -8,7 +8,8 @@ namespace Dsi.GitUtil.Security {
     
     public class Tokens {
 
-        public static string GitHub = "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";
+        //public static string GitHub = "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";
+        public static string GitHub = "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";
 
     }
 }
